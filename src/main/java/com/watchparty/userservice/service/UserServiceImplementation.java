152893897d81a41b8e6ebe086a9f172d1c81c831
Jesse Korolainen <jesse.korolainen@hotmail.com>
2022-03-27 15:40:08 +0200

    Access and Refresh token working

--- src/main/java/com/watchparty/userservice/service/UserServiceImplementation.java
+++ src/main/java/com/watchparty/userservice/service/UserServiceImplementation.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -21,6 +22,7 @@ import java.util.List;
 public class UserServiceImplementation implements UserService, UserDetailsService {
     private final UserRepo userRepo;
     private final RoleRepo roleRepo;
+    private final PasswordEncoder passwordEncoder;
 
     // Get user details from database
     @Override
@@ -43,6 +45,7 @@ public class UserServiceImplementation implements UserService, UserDetailsServic
     @Override
     public User saveUser(User user) {
         log.info("Saving user {} to the database", user.getName());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepo.save(user);
     }
 
